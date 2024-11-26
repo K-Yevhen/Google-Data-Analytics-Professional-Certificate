c88a5d729b095d67144b81184655f00f013a6cb6
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2024-11-26 14:30:26 +0100

    Created simple data viz with ggplot

---
+++ Data-Analysis-with-R-Programming/Module-4/penguins_viz.R
@@ -0,0 +1,9 @@
+data("penguins")
+View(penguins)
+
+library(ggplot2)
+library(tidyverse)
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = bill_length_mm, y = bill_depth_mm))
